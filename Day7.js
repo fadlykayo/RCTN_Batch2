185f33dbeeaf2e313cfd0f32a8c2655e7c1effef
Fadly Kayo <fadly.kayo@rebelworks.co>
2018-11-22 19:54:41 +0700

    Add material day 7

---
+++ Day7.js
@@ -0,0 +1,8 @@
+/*
+Resourceful links for this session:
+https://reactjs.org/docs/react-component.html
+https://codepen.io/gaearon/pen/wqvxGa?editors=0010
+https://github.com/axios/axios
+https://www.sitepoint.com/axios-beginner-guide/
+http://codeheaven.io/how-to-use-axios-as-your-http-client/
+*/

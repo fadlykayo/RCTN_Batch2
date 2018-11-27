9f7f64c99174fa6cb8aaf3e613dca819d82e9035
Fadly Kayo <fadly.kayo@rebelworks.co>
2018-11-27 19:09:52 +0700

    Add day 8 materials

---
+++ Day8.js
@@ -0,0 +1,7 @@
+/*
+Resourceful links for this session:
+https://github.com/mui-org/material-ui
+https://github.com/mui-org/material-ui/blob/master/examples/create-react-app/src/pages/index.js
+https://v0.material-ui.com/#/customization/styles
+https://github.com/emotion-js/emotion
+*/

62848c475630c860d4601b7d6fcf8b9d9949c2df
Fadly Kayo <fadly.kayo@rebelworks.co>
2018-12-06 19:02:16 +0700

    Add Day 13 materials

---
+++ Day13.js
@@ -0,0 +1,6 @@
+/*
+Resourceful links for this session:
+https://jest-bot.github.io/jest/docs/expect.html
+https://www.w3schools.com/jsref/dom_obj_all.asp
+https://airbnb.io/enzyme/docs/api/render.html
+*/
